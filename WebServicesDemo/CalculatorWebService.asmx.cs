babc56432bff9d26e8ab7b6659648819a755e9dd
Emrullah <Emrullah@EXCALIBUR-G770>
2022-12-26 13:16:10 +0300

    Add project files.

---
+++ WebServicesDemo/CalculatorWebService.asmx.cs
@@ -0,0 +1,24 @@
+﻿using System.Web.Services;
+
+namespace WebServicesDemo
+{
+    /// <summary>
+    /// Summary description for CalculatorWebService
+    /// </summary>
+    [WebService(Namespace = "http://tempuri.org/")]
+    [WebServiceBinding(ConformsTo = WsiProfiles.BasicProfile1_1)]
+    [System.ComponentModel.ToolboxItem(false)]
+   
+    // [System.Web.Script.Services.ScriptService]
+    public class CalculatorWebService : WebService
+    {
+
+        [WebMethod]
+        public string Add(int firstNumber, int secondNumber)
+        {
+            var toplam = firstNumber + secondNumber;
+
+            return "Toplam Sonuç: "+toplam;
+        }
+    }
+}
